555957ade5111fd7f66ff68e471ac3a87076d36c
ivan <vansmuhammad@gmail.com>
2019-11-15 08:59:33 +0700

    add: custom components

---
+++ src/custom/spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { SpinnerContainer, SpinnerOverlay } from './styles'
+
+const SpinnerComponent = () => {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  )
+}
+
+export default SpinnerComponent
